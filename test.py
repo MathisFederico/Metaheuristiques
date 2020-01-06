3226ad140cac936c04e7a22ca369efecb2148e4b
Emile Clastres <emile.clastres@student-cs.fr>
2020-01-06 12:11:13 +0100

    instances + get_dict

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-a = 2
 
-print(a)
+def get_dict(instance_name = "n20w20.001.txt"):
+    with open('DumasEtAl/' + instance_name, "r") as f:
+        d = {}
+        for _ in range(6):
+            f.readline()
+        for row in f:
+            r = [x for x in row.split(' ') if x != '']
+            key = int(r[0])
+            if key != 999:
+                d[key] = {'x':int(float(r[1])), 'y':int(float(r[2])), 'ti':int(float(r[4])), 'tf':int(float(r[5])), 'demand':int(float(r[3])), 't_service':int(float(r[-1][:-2]))}
+    return d
+
+    
+
+d = get_dict()
+print(d)
