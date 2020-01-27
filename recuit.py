7a81294ce603e5991d378d5ac53d50879e9aa611
Mathïs <mathfederico@gmail.com>
2020-01-27 01:52:10 +0100

    Corrected recuit

--- recuit.py
+++ recuit.py
@@ -1,76 +1,93 @@
-from utilities import get_dict, plot_sol, max_dist, dist
+from utilities import get_dict, plot_sol, max_dist, dist, draw_animated_solution
 from energy import Potential
 from random import randint, random
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import copy
+from greedy import fifo_greedy
 
-def recuit(T = 100., T_min = 10., lamb = 0.99, pot = Potential(), plotting = False, inst_dict = get_dict(), print_results = True, print_log = False):
-    λ = lamb
-
-    def voisin1(solution):
-        """Renvoie la solution avec deux noeuds aléatories échangés"""
-        i = randint(1, len(solution)-1)
-        j = randint(1, len(solution)-1)
-        while j==i:
-            j = randint(1, len(solution)-1)
-        s_perm = [k for k in solution]
-        s_perm[i],s_perm[j] = s_perm[j],s_perm[i]
-        return s_perm
+def recuit(inst_dict, pot, T = 100., T_min = 10., lamb = 0.99, initial_key=1, **kwargs):
     
+    greedy_start = kwargs.get('greedy_start', True)
+    plotting = kwargs.get('plotting', True)
+    print_results = kwargs.get('print_results', True)
+    print_log = kwargs.get('print_log', True)
+
     def voisin(solution):
         """Renvoie la solution avec deux noeuds aléatories échangés"""
         i = randint(1, len(solution)-2)
-        s_perm = [k for k in solution]
-        s_perm[i],s_perm[i+1] = s_perm[i+1],s_perm[i]
-        return s_perm 
+        s_perm = copy(solution)
+        s_perm[i], s_perm[i+1] = s_perm[i+1], s_perm[i]
+        return s_perm
 
-    s = list(range(1, len(inst_dict)+1)) #solution initiale 1, 2, ... , n
-    penalite = (len(s)+1)/2*max_dist(inst_dict)
+    # We take a greedy initial solution
+    solution = list(range(1, len(inst_dict)+1)) #solution initiale 1, 2, ... , n
+    if greedy_start:
+        solution = fifo_greedy(inst_dict)
+    penality = (len(solution)+1)*max_dist(inst_dict)/10
+    print(penality)
 
-    def cost1(E, penalite = penalite):
-        return 10*E[0] + E[1] + penalite*int(E[0]> 0)
+    def cost(E, penality=penality):
+        return E[0] + E[1] + E[2]*penality
     
-    def cost(E, penalite = penalite):
-        return E[2]/len(s)
-    
-    s_best = s
+    best_solution = solution
+
+    evaluation = pot.evaluate(inst_dict, solution, initial_key=initial_key)
+    energy = cost(evaluation)
+    best_evaluation, energy_best = copy(evaluation), copy(energy)
+
+    solutions_energy = []
+    best_energies = []
+    print(f'Initial energy: {energy}')
+
     k = 0
-    E = pot.evaluate(inst_dict, s, initial_key=1)
-    e = cost(E)
-    E_best = E
-    e_best = cost(E_best)
-    e_s = []
-    e_bests = []
-    print(f'cost initial : {cost(E)}')
     while T > T_min:
-        s_new = voisin(s)
-        E_new = pot.evaluate(inst_dict, s_new, initial_key=1)
-        e_new = cost(E_new)
-        Δe = e_new-e
-        P = np.exp(-Δe/T)
-        tirage_random = random()
-        if Δe < 0 :
-            if e_new < e_best:
-                E_best, e_best, s_best = E_new,e_new, s_new
-        if Δe < 0 or tirage_random < P :
-            E, s = E_new, s_new
 
-        #print(f'T : {T}, λ*T : {λ*T} , T-λ*T : {T - λ*T}')
-        T = λ*T
-        E,e, s = E_new,e_new, s_new
-        k+=1
-        e_s.append(e*len(s))
-        e_bests.append(e_best*len(s))
-        if print_log : print(f'iteration {k} : E = {int(E[0]), int(E[1])}, E_best = {int(E_best[0]), int(E_best[1])}')
+        # We evaluate a neighboring solution
+        new_solution = voisin(solution)
+        new_evaluation = pot.evaluate(inst_dict, new_solution, initial_key=initial_key)
+        new_energy = cost(new_evaluation)
+
+        energy_gap = new_energy - energy
+
+        # If the energy is lower we keep the exemple
+        if energy_gap < 0 :
+            # If the explored solution is the new best, we save it
+            if new_energy < energy_best:
+                best_evaluation, energy_best, best_solution = new_evaluation, new_energy, new_solution
+            evaluation, energy, solution = new_evaluation, new_energy, new_solution
+        
+        # Else we still have a chance to keep it determined by temperature
+        P = np.exp(-energy_gap/T)
+        if random() < P :
+            evaluation, energy, solution = new_evaluation, new_energy, new_solution
+        
+        # We decrease the temperature by the decay factor lamb
+        T *= lamb
+
+        # We save and print informations
+        k += 1
+        solutions_energy.append(energy)
+        best_energies.append(energy_best)
+        if print_log : print(f'Iteration {k} :\t errors = {evaluation[2]} \t P={P} \t {energy} \t {new_energy}')
+
     if print_results:
-        print(f's_best = {s_best} \n ______________________ \n E_best = {int(E_best[0]), int(E_best[1])} en {k} iterations')
+        print(f'Best = {best_solution} \nEvaluation = {best_evaluation} in {k} iterations')
+
     if plotting :
-        plt.plot(e_s, label = 'cost function')
-        plt.plot(e_bests, label = 'best solution')
+        plt.plot(solutions_energy, label = 'Solutions energy')
+        plt.plot(best_energies, label = 'Best_energies')
+        plt.plot([best_evaluation[1] for _ in best_energies], linestyle='--', label = 'Travel time', color='g')
+        plt.plot([best_evaluation[1] + best_evaluation[2]*penality for _ in best_energies], linestyle='--', label = 'Penality', color='r')
+        plt.plot([best_evaluation[1] + best_evaluation[2]*penality + best_evaluation[0] for _ in best_energies], linestyle='--', label = 'Out window', color='b')
         plt.legend()
         plt.show()
-    return s_best,E_best
+
+    return best_solution, best_evaluation
 
 if __name__ == "__main__":
-    s,E = recuit(T = 10, T_min = 0.1, lamb = 0.9999, plotting = True, pot = Potential(), inst_dict = get_dict(), print_results = True, print_log = False)
-    plot_sol(get_dict(), s)
+    pot = Potential()
+    data = get_dict("n60w20.002.txt")
+    solution, energy = recuit(data, pot, T=100, T_min=1, lamb=0.999, plotting=True, print_log=True, greedy_start=True)
+    draw_animated_solution(data, [solution])
+    print("Distances evaluations :", pot.dist_count)
